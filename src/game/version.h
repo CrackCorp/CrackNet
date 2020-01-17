8eddbe364d5c3e95e47d97205f8e7c3effce84e6
ChillerDragon <ChillerDragon@gmail.com>
2020-01-17 19:57:58 +0100

    Set version to 0.7

--- src/game/version.h
+++ src/game/version.h
@@ -2,8 +2,9 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.6.4, 12.8"
-#define GAME_NETVERSION "0.6 626fce9a778df4d4"
+#define GAME_VERSION "0.7.4, 12.8"
+#define GAME_NETVERSION_HASH_FORCED "802f1be60a05665f"
+#define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH_FORCED
 #define GAME_RELEASE_VERSION "12.8"
 #define CLIENT_VERSIONNR 12080
 extern const char *GIT_SHORTREV_HASH;
